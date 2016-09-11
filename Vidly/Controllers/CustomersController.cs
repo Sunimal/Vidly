5d876a306037baa6b917338224e649b394bfa91a
Sunimal <sunimalherath@gmail.com>
2016-09-12 08:36:17 +1000

    Rename Customer Create action to Save

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -53,7 +53,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
             _context.Customers.Add(customer);
             _context.SaveChanges();
