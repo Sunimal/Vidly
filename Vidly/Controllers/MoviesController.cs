d2df08f8c4169432540972681989e885017662a4
Sunimal <sunimalherath@gmail.com>
2016-09-12 05:37:20 +1000

    Adding code to show extra detail in the movie details page.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -63,7 +63,7 @@ namespace Vidly.Controllers
 
         public ActionResult Details(int Id)
         {
-            var movie = _context.Movies.SingleOrDefault(c => c.Id == Id);
+            var movie = _context.Movies.Include( c => c.Genre).SingleOrDefault(c => c.Id == Id);
             return View(movie);
         }        
     }
