38fc22c8fc13cc830aa4374b8ec368ca0d9743d5
Sunimal <sunimalherath@gmail.com>
2016-09-12 02:01:00 +1000

    Add ApplicationDbContext to the CustomersController to access data from the database

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -9,30 +9,44 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            //var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.id == id);
+            //var customer = GetCustomers().SingleOrDefault(c => c.id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.id == id);
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {id = 1, Name = "John Doe" },
-                new Customer {id = 2, Name = "Jane Doe" }
-            };
-        }
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer {id = 1, Name = "John Doe" },
+        //        new Customer {id = 2, Name = "Jane Doe" }
+        //    };
+        //}
     }
 }
