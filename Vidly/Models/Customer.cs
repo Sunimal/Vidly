4c2265eb96d1439445a57a173706f0835760652c
Sunimal <sunimalherath@gmail.com>
2016-09-12 01:42:42 +1000

    Insert records to the membershiptype table

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Vidly.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Customer
     {
         public int id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
